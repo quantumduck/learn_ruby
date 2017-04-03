db74df0e8dd3d1f679c6ea0b3f60d11818fefdde
quantumduck <quantum.duck@hotmail.com>
2017-04-03 16:28:47 -0400

    more of 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -16,3 +16,17 @@ end
 def start_of_word(str, n)
   str.to_s[0, n]
 end
+
+def first_word(str)
+  str.split.first
+end
+
+def titleize(str)
+  str.capitalize
+end
+
+def test_titleize_title_case(str)
+  result = str.first_word.capitalize
+  str.split[1, str.split.length].each { |s| result += ' ' + s.capitalize }
+  result
+end
